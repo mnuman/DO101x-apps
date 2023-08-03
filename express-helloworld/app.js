db234ea8e0047f4d3b8a69d8ad85fac21931935b
Milco Numan <milco.numan@gmail.com>
2023-08-03 11:32:52 +0200

    Add /mars endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,9 +2,11 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello peoples!\n');
 });
-
+app.get('/mars', function (req, res) {
+  res.send('Hello Martians!\n');
+});
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
